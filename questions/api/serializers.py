3cd31afb20f5f062e501390a501eebbe98c33235
矢野大樹 <daikitech0123@gmail.com>
2019-12-25 22:45:19 +0200

    Add answer

--- questions/api/serializers.py
+++ questions/api/serializers.py
@@ -6,17 +6,21 @@ class AnswerSerializer(serializers.ModelSerializer):
     created_at = serializers.SerializerMethodField()
     likes_count = serializers.SerializerMethodField()
     user_has_voted = serializers.SerializerMethodField()
+    question_slug = serializers.SerializerMethodField()
+
 
     class Meta:
         model = Answer
-        exclude = ["question","voters","updated_at"]
+        exclude = ["question","votes","updated_at"]
     def get_created_at(self,instance):
-            return instance.created_at.strftime("%B %d %Y")
+            return instance.created_at.strftime("%B %d, %Y")
     def get_likes_count(self,instance):
-            return instance.voters.count()
+            return instance.votes.count()
     def get_user_has_voted(self,instance):
             request = self.context.get("request")
-            return instance.voters.filter(pk=request.user.pk).exists()
+            return instance.votes.filter(pk=request.user.pk).exists()
+    def get_question_slug(self, instance):
+        return instance.question.slug
         
 class QuestionSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField(read_only=True)
